48777b81ea0b1c13837513cf9237d95b7db20137
XogosAquiDEV <olimial03@gmail.com>
2024-10-17 09:25:46 +0200

    Update audio _bosque _ kokiri.gd

--- Scripts/audio _bosque _ kokiri.gd
+++ Scripts/audio _bosque _ kokiri.gd
@@ -1,14 +1,14 @@
 extends AudioStreamPlayer
 
-
+#inicia la musica
 func _ready():
 	play()
-
+#mantiene la musica encendida
 func play_music():
 	if not is_playing():
 		play()
 
-
+#para la musica cuando estea sonando
 func stop_music():
 	if is_playing():
 		stop()
