51caf58a2bed02d920ba60bdf8f5edd4858d9f13
XogosAquiDEV <olimial03@gmail.com>
2024-10-17 09:24:54 +0200

    Update Musica_menu.gd

--- Scripts/Musica_menu.gd
+++ Scripts/Musica_menu.gd
@@ -2,15 +2,15 @@ extends AudioStreamPlayer2D
 
 
 
-
+#Inicia la musica
 func _ready():
 	play()
-
+#para que se reinicie si no suena o se acaba
 func play_music():
 	if not is_playing():
 		play()
 
-
+#para la musica  
 func stop_music():
 	if is_playing():
 		stop()
